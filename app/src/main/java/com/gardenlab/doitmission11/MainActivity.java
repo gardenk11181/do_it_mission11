e158bc4057a6acefb808ffc1c78e5df99a364e50
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-23 19:40:22 +0900

    Finish

--- app/src/main/java/com/gardenlab/doitmission11/MainActivity.java
+++ app/src/main/java/com/gardenlab/doitmission11/MainActivity.java
@@ -2,13 +2,48 @@ package com.gardenlab.doitmission11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        editText = findViewById(R.id.editText);
+
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentService = new Intent(getApplicationContext(),MyService.class);
+                intentService.putExtra("String",editText.getText().toString());
+                startService(intentService);
+            }
+        });
+
+        Intent intent = getIntent();
+        processIntent(intent);
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        processIntent(intent);
+        super.onNewIntent(intent);
+    }
+
+    public void processIntent(Intent intent) {
+        String msg = intent.getStringExtra("String");
+        text.setText(msg);
     }
 }
